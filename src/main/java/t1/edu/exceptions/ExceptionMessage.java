0829179480fe852fb87d450e7aa336f0a81d4ef7
Antonov A.S <java.dev.antonov.a.s@gmail.com>
2025-03-31 14:47:34 +0100

    fix: Добавлен сваггер к эксепешен мессаджу

--- src/main/java/t1/edu/exceptions/ExceptionMessage.java
+++ src/main/java/t1/edu/exceptions/ExceptionMessage.java
@@ -1,5 +1,6 @@
 package t1.edu.exceptions;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
+@Schema(description = "Ответ на исключение/**/")
 public class ExceptionMessage {
     private String code;
     private String message;
